1d57b7a437ccf550015554dd312f58b7662a91b4
ntvsp <nguyentuanvuabc@gmail.com>
2025-05-17 22:49:22 +0800

    feat: update ground animation

--- assets/script/ground.ts
+++ assets/script/ground.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, UITransform, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('ground')
@@ -22,12 +22,78 @@ export class ground extends Component {
     })
     public ground3: Node;
 
+    // create ground width variables
+
+    public groundWidth1: number;
+    public groundWidth2: number;
+    public groundWidth3: number;
+
+    public tempStartLocation1 = new Vec3;
+    public tempStartLocation2 = new Vec3;
+    public tempStartLocation3 = new Vec3;
+
+    public gameSpeed: number = 50;
+
+    protected onLoad(): void {
+        this.startUp();
+    }
+
+    private initGroundWidths() {
+        this.groundWidth1 = this.ground1.getComponent(UITransform).width;
+        this.groundWidth2 = this.ground2.getComponent(UITransform).width;
+        this.groundWidth3 = this.ground3.getComponent(UITransform).width;
+    }
+
+    private setInitialGroundPositions() {
+        this.tempStartLocation1.x = 0;
+        this.tempStartLocation2.x = this.groundWidth1;
+        this.tempStartLocation3.x = this.groundWidth1 + this.groundWidth2;
+
+        this.ground1.setPosition(this.tempStartLocation1);
+        this.ground2.setPosition(this.tempStartLocation2);
+        this.ground3.setPosition(this.tempStartLocation3);
+    }
+
+    startUp() {
+        this.initGroundWidths();
+        this.setInitialGroundPositions();
+    }
+
     start() {
 
     }
 
-    update(deltaTime: number) {
+    private updateGroundPositions(deltaTime: number) {
+        this.tempStartLocation1 = this.ground1.getPosition();
+        this.tempStartLocation2 = this.ground2.getPosition();
+        this.tempStartLocation3 = this.ground3.getPosition();
+        this.tempStartLocation1.x -= this.gameSpeed * deltaTime;
+        this.tempStartLocation2.x -= this.gameSpeed * deltaTime;
+        this.tempStartLocation3.x -= this.gameSpeed * deltaTime;
 
+        this.ground1.setPosition(this.tempStartLocation1);
+        this.ground2.setPosition(this.tempStartLocation2);
+        this.ground3.setPosition(this.tempStartLocation3);
+    }
+
+    private resetGroundIfNeeded() {
+        if (this.tempStartLocation1.x <= -this.groundWidth1) {
+            this.tempStartLocation1.x = this.tempStartLocation3.x + this.groundWidth3;
+            this.ground1.setPosition(this.tempStartLocation1);
+        }
+        if (this.tempStartLocation2.x <= -this.groundWidth2) {
+            this.tempStartLocation2.x = this.tempStartLocation1.x + this.groundWidth1;
+            this.ground2.setPosition(this.tempStartLocation2);
+        }
+        if (this.tempStartLocation3.x <= -this.groundWidth3) {
+            this.tempStartLocation3.x = this.tempStartLocation2.x + this.groundWidth2;
+            this.ground3.setPosition(this.tempStartLocation3);
+        }
+    }
+
+    update(deltaTime: number) {
+        this.updateGroundPositions(deltaTime);
+        this.resetGroundIfNeeded();
     }
 }
 
