ecf89b4f64940386adbe12fee6b9241760012301
ntvsp <nguyentuanvuabc@gmail.com>
2025-05-17 22:58:34 +0800

    update: reset ground 1

--- assets/script/ground.ts
+++ assets/script/ground.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, UITransform, Vec3 } from 'cc';
+import { _decorator, Canvas, Component, director, Node, UITransform, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('ground')
@@ -76,6 +76,22 @@ export class ground extends Component {
         this.ground3.setPosition(this.tempStartLocation3);
     }
 
+    /**
+     * So sánh hiệu năng giữa resetGroundIfNeeded (v1) và resetGroundIfNeededV2:
+     * 
+     * - resetGroundIfNeeded (v1):
+     *   + Khi ground ra khỏi màn hình trái, sẽ đặt nó ngay sau ground cuối cùng (dựa vào vị trí các mảnh ground còn lại).
+     *   + Chỉ thao tác trên biến đã có, không truy cập scene/canvas.
+     *   + Hiệu năng tốt hơn vì không phải lấy thông tin từ scene mỗi frame.
+     * 
+     * - resetGroundIfNeededV2:
+     *   + Khi ground ra khỏi màn hình trái, sẽ đặt lại vị trí x = chiều rộng canvas (mép phải màn hình).
+     *   + Mỗi lần gọi đều phải truy cập scene, lấy component canvas và width của canvas.
+     *   + Hiệu năng kém hơn do phải truy cập scene/canvas liên tục mỗi frame.
+     * 
+     * Kết luận: Nên dùng resetGroundIfNeeded (v1) cho hiệu năng tốt hơn, đặc biệt khi update() được gọi liên tục mỗi frame.
+     **/
+
     private resetGroundIfNeeded() {
         if (this.tempStartLocation1.x <= -this.groundWidth1) {
             this.tempStartLocation1.x = this.tempStartLocation3.x + this.groundWidth3;
@@ -87,6 +103,24 @@ export class ground extends Component {
         }
         if (this.tempStartLocation3.x <= -this.groundWidth3) {
             this.tempStartLocation3.x = this.tempStartLocation2.x + this.groundWidth2;
+            this.ground3.setPosition(this.tempStartLocation3);
+        }
+    }
+
+    private resetGroundIfNeededV2() {
+        const scense = director.getScene();
+        const canvas = scense.getComponentInChildren(Canvas)
+
+        if (this.tempStartLocation1.x <= -this.groundWidth1) {
+            this.tempStartLocation1.x = canvas.getComponent(UITransform).width;
+            this.ground1.setPosition(this.tempStartLocation1);
+        }
+        if (this.tempStartLocation2.x <= -this.groundWidth2) {
+            this.tempStartLocation2.x = canvas.getComponent(UITransform).width;
+            this.ground2.setPosition(this.tempStartLocation2);
+        }
+        if (this.tempStartLocation3.x <= -this.groundWidth3) {
+            this.tempStartLocation3.x = canvas.getComponent(UITransform).width;
             this.ground3.setPosition(this.tempStartLocation3);
         }
     }
