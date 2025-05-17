a37d3146922f9926e022adf44da79676f8df050f
ntvsp <nguyentuanvuabc@gmail.com>
2025-05-17 22:29:40 +0800

    update ground

--- assets/script/ground.ts
+++ assets/script/ground.ts
@@ -3,12 +3,31 @@ const { ccclass, property } = _decorator;
 
 @ccclass('ground')
 export class ground extends Component {
+
+    @property({
+        type: Node,
+        tooltip: "Ground 1 is here",
+    })
+    public ground1: Node;
+
+    @property({
+        type: Node,
+        tooltip: "Ground 2 is here",
+    })
+    public ground2: Node;
+
+    @property({
+        type: Node,
+        tooltip: "Ground 3 is here",
+    })
+    public ground3: Node;
+
     start() {
 
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
 
